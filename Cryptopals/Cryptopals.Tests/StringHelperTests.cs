839870ded8f5b55b7a0cbd2d701778fe9f01a3b6
Andrey Stukalin <a.stukalin@crossvertise.com>
2016-05-17 16:47:09 +0200

    initial commit wth hex string parser

---
+++ Cryptopals/Cryptopals.Tests/StringHelperTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Cryptopals.Tests
+{
+    using System.Linq;
+
+    using Cryptopals.Util;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class StringHelperTests
+    {
+        [Test]
+        public void ParseTest_PassString_GetArray()
+        {
+            var s = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+            var res = s.ParseHex();
+
+            Assert.That(res.Length, Is.EqualTo(s.Length / 2));
+            Assert.That(0x49, Is.EqualTo(res.First()));
+            Assert.That(0x6d, Is.EqualTo(res.Last()));
+        }
+    }
+}
---
+++ Cryptopals/Cryptopals.Util/StringHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace Cryptopals.Util
+{
+    using System.Globalization;
+
+    public static class StringHelper
+    {
+        /// <summary>
+        /// converts string containing encoded bytes into bytearray
+        /// </summary>
+        public static byte[] ParseHex(this string hexstring)
+        {
+            byte[] result = new byte[hexstring.Length / 2];
+
+            for (int i = 0; i < hexstring.Length; i += 2)
+            {
+                result[i / 2] = byte.Parse(hexstring.Substring(i, 2), NumberStyles.HexNumber);
+            }
+
+            return result;
+        }
+    }
+}
